da8afa3284abde3aa16edada1e6d314a76d4c289
suzhaomin <yfszm666@outlook.com>
2022-08-12 02:25:40 +0800

    day1 learning

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter';
+import Counter2 from './components/counter2';
+import CounterActionHandle from './components/counterActionHandle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CounterActionHandle/>
   </React.StrictMode>
 );
 
