d93de3ef466d34d49088e47fafc0f912d7cc6f09
suzhaomin <yfszm666@outlook.com>
2022-08-19 01:38:03 +0800

    day2 learning

--- src/components/counterActionHandle.jsx
+++ src/components/counterActionHandle.jsx
@@ -4,31 +4,44 @@ class CounterActionHandle extends Component {
   // state it has all the props we need
   state = {
     // in React, we use const, mean constant
-    count: 0,
+    value: this.props.counter.value,
     // imageUrl:'https://picsum.photos/200'
     tag:["tag1","tag2","tag3"]
   };
   // from here, please remember to google bind(), I dont understand now.
-  constructor(){
-    super();
-    this.handleIncreament = this.handleIncreament.bind(this);
+//   constructor(props){
+//     super(props);
+//     this.state={
+//         value:this.props.value
+//     }
+//     this.handleIncreament = this.handleIncreament.bind(this);
+//   }
+  handleIncreament=product=>{
+    console.log('Increament clicked',this,product);
+    // If we want to use this.state.count++ to increase the count, it will not work, coz react doesnt konw what is the change, we should use below way to update state
+    // this way will sysc to update state
+    this.setState({value: this.state.value +1})
   }
-  handleIncreament(){
-    console.log('Increament clicked',this);
+
+  dohandleIncreament=()=>{
+    this.handleIncreament({id:1})
   }
   render() {
     this.getBadgeClasses();
-
+    console.log("props",this.props);
     return (
+        
       // if we dont want to see the <div> inside the root dev, then just use React.Fragment
       <React.Fragment>
+        <h4>{this.props.id}</h4>
         {/* <img src ={this.state.imageUrl} alt=""/> */}
         {/* why we dont use class? because in javascript we had this class, we got to make diff */}
         {/* if you want to learn why I write this, learn boostrap */}
         <span className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button onClick={this.handleIncreament} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={()=>this.handleIncreament({id:1})} className="btn btn-secondary btn-sm">Increment</button>
+        <button className="btn btn-danger btn-sm m-2" onClick={()=>this.props.onDelete(this.props.counter.id)}>Delete</button>
         <ul>{this.state.tag.map(tag=><li key={tag}>{tag}</li>)}</ul>
       </React.Fragment>
     );
@@ -36,14 +49,14 @@ class CounterActionHandle extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
   formatCount() {
     // below case is equal to this.state.count
-    const { count } = this.state;
-    return count === 0 ? <h1>ZERO</h1> : this.state.count;
+    const { value: count } = this.state;
+    return count === 0 ? <h1>ZERO</h1> : this.state.value;
   }
 }
 export default CounterActionHandle;
--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Counter from './components/counter';
 import Counter2 from './components/counter2';
 import CounterActionHandle from './components/counterActionHandle';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounterActionHandle/>
+    <Counters/>
   </React.StrictMode>
 );
 
